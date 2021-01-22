de3bc2fbc36607de1333858da133651ad303a823
Ryan Glasnapp <ryan.glasnapp@gmail.com>
2021-01-22 12:01:45 -0500

    Added translation
    Got google translate package working and translating the a column from
    the sample data.

--- Sakura_Data_Processing.py
+++ Sakura_Data_Processing.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -18,10 +19,21 @@
 # My intent is to process the data (and likely translate) the data into a CSV that I can then load in a separate notebook for analysis and prediction.
 #
 
+# +
 import pandas as pd
 import numpy as np
 
+from googletrans import Translator
+# -
 
-pd.read_csv('sample_sakura_pref/biz_sakura_yosou_20190207_01.csv', encoding='SHIFT_JIS')
+sample = pd.read_csv('sample_sakura_pref/biz_sakura_yosou_20190207_01.csv', encoding='SHIFT_JIS', header=None)
+
+sample
+
+sample[1][0]
+
+translator = Translator()
+sample['Location'] = sample[1].apply(translator.translate).apply(getattr, args=('text',))
 
 
+sample
