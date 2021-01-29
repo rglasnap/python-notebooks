8ee1cd71bd94235b4482b0267ea95fd1cbeb7bbb
Ryan Glasnapp <ryan.glasnapp@gmail.com>
2021-01-29 14:03:29 -0500

    More progress on fixed width formatting
    Working through dealing with the fixed width formatting of the data.
    I have the data loaded, but still working on cleaning it up,
    getting rid of the duplicate header rows, and converting the month and
    day to actual month and day.

--- Sakura_Data_Processing_JMA.py
+++ Sakura_Data_Processing_JMA.py
@@ -28,24 +28,37 @@ from bs4 import BeautifulSoup
 import lxml
 from io import StringIO
 
+# Workaround because I'm on an old version of pandas
+pd.set_option("display.max_colwidth", 10000)
+
+
 bloom_req = requests.get('https://www.data.jma.go.jp/sakura/data/sakura003_06.html')
 
 bloom_content = BeautifulSoup(bloom_req.content, 'lxml')
 
+# +
+colspecs = [(0,5),(5,9),(9,16),(16,23),(23,30),(30,37),(37,44),(44,51),(51,58),(58,65),(65,72),(72,80),(80,87),(87,None)]
 #print(bloom_content.find(id='main').pre.text)
 bloom_string = StringIO(bloom_content.find(id='main').pre.text)
-print(bloom_string.getvalue())
+#print(bloom_string.getvalue())
 #derp = pd.read_csv(bloom_string,sep=r"[ ]{2,}", engine='c')
-unparsed_data = pd.read_csv(bloom_string,header=None)
+unparsed_data = pd.read_fwf(bloom_string,header=0,colspecs=colspecs,
+                           parse_dates=['2011','2012'],infer_datetime_format=True,
+                           true_values=['*'])
+
+unparsed_data.head()
+# -
 
 unparsed_data[unparsed_data.duplicated()]
 
 unparsed_data = unparsed_data.drop_duplicates()
+#print(unparsed_data.iloc[5:8].apply(lambda x: x.to_string(index=False)))
 
 raw_headers = StringIO(unparsed_data.loc[:1,0].to_string(index=False))
 raw_data = StringIO(unparsed_data.loc[2:,0].to_string(index=False))
-test_data = pd.read_fwf(raw_data,header=None)
+print(raw_data.getvalue())
+test_data = pd.read_fwf(raw_data,header=None,colspecs=colspecs)
 
-test_data.head()
+test_data.head(10)
 
 
